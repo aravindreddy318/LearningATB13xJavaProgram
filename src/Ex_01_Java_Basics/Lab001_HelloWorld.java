9f64ada38e475aa1ecacf14751482a2972a5d046
aravindreddy318 <kanugantiaravindreddy@gmail.com>
2025-06-09 22:48:29 +0530

    first commit

---
+++ src/Ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,7 @@
+package Ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am running Java Program");
+    }
+}
---
+++ src/Ex_01_Java_Basics/ex_02_java.java
@@ -0,0 +1,4 @@
+package Ex_01_Java_Basics;
+
+public class ex_02_java {
+}
